7db29e62aea5c9251e33fbcb66e180383a8f88b1
Alian713 <divy1211.dc@gmail.com>
2022-10-29 19:27:41 +0800

    Removed testing code from wrong file

--- src/classes/XsInt.py
+++ src/classes/XsInt.py
@@ -1,23 +1,6 @@
 from src.classes.XsObject import XsObject
-from src.enums.XsType import XsType
 
 
 class XsInt(XsObject, int):
     def __init__(self, val, op = None):
         super().__init__(op)
-
-
-class Ctx:
-    x = XsObject(XsType.INT)
-    y = XsObject(XsType.INT)
-    z = XsObject(XsType.INT)
-
-a = Ctx()
-
-a.x = XsInt(1)
-a.y = XsInt(2)
-a.z = a.x
-a.z = (a.z+a.x)/a.y
-a.x = a.z
-a.x = XsInt(10)
-
